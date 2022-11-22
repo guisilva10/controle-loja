741e09f6e60e7b132e08c0033b34439f87471afd
guisilva10 <107316736+guisilva10@users.noreply.github.com>
2022-11-22 02:03:36 -0300

    projeto concluido

---
+++ src/components/Header.js
@@ -0,0 +1,57 @@
+import {
+  Avatar,
+  Flex,
+  HStack,
+  Icon,
+  IconButton,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import React from "react";
+import { useSidebarContext } from "../contexts/SidebarContext";
+import { FiMenu } from "react-icons/fi";
+import avatar from '../assets/avatar.png'
+
+const Header = () => {
+  const isMobile = useBreakpointValue({
+    base: true,
+    lg: false,
+  });
+
+  const { onOpen } = useSidebarContext();
+
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxW={1120}
+      h="20"
+      mx="auto"
+      px="2"
+      py="2"
+      align="center"
+      boxShadow="0 1px 0 #ccc"
+      color="gray.500"
+      fontWeight="bold"
+    >
+      {isMobile && (
+        <IconButton
+          icon={<Icon as={FiMenu} />}
+          onClick={onOpen}
+          variant="unstyled"
+          fontSize="20"
+          mr="2"
+        ></IconButton>
+      )}
+      <Text>LOGO</Text>
+      <Flex ml="auto">
+        <HStack>
+          <Text>Guilherme Silva</Text>
+          <Avatar size="md" name="Guilherme Silva" />
+        </HStack>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Header;
